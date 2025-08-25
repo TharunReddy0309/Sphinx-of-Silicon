322a3315db8183a4753b3eb199e91301c010a3d3
TharunReddy <bhumireddytharunreddy9@gmail.com>
2025-08-25 20:30:37 +0530

    Update game.py

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ chat_model_groq = ChatGroq(
     groq_api_key=st.secrets["groqai"],
     temperature=0.9,
     model_kwargs={"top_p": 0.9},
-    model="openai/gpt-oss-120b"  # or llama-4-scout
+    model="openai/gpt-oss-120b"  
 )
 
 # ---------------- Generate Secret Phrase ----------------
